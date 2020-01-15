38b5f20702db5cba7d5cc757b6229a48c09c848c
Philip Zhang <zhangxilai@doublechaintech.com>
2020-01-15 23:36:00 +0800

    initial version

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+rsync -avz ./*.py  -e "ssh -p 4422" philip@t420.doublechaintech.cn:~/resin-3.1.12/webapps/ROOT/upload/
+#scp -P 4422 *.py   philip@t420.doublechaintech.cn:~/resin-3.1.12/webapps/ROOT/upload/
+echo "upload done"
+ssh -p 4422 philip@t420.doublechaintech.cn "cd ~/resin-3.1.12/webapps/ROOT/upload && python3 dectect.py "
+open http://t420.doublechaintech.cn:2080/upload/output.jpg
